a7f35100f5c2b9a8681a95ba9d81f4fed24bd3e0
Yuta Ide <yIde4949@gmail.com>
2016-10-29 17:34:46 +0900

    [update] api v1 ctr

--- sadness_ojisan/groove_server/app/controllers/api/v1/records_controller.rb
+++ sadness_ojisan/groove_server/app/controllers/api/v1/records_controller.rb
@@ -1,74 +1,64 @@
-class RecordsController < ApplicationController
-  before_action :set_record, only: [:show, :edit, :update, :destroy]
-
-  # GET /records
-  # GET /records.json
-  def index
-    @records = Record.all
-  end
-
-  # GET /records/1
-  # GET /records/1.json
-  def show
-  end
-
-  # GET /records/new
-  def new
-    @record = Record.new
-  end
-
-  # GET /records/1/edit
-  def edit
-  end
-
-  # POST /records
-  # POST /records.json
-  def create
-    @record = Record.new(record_params)
-
-    respond_to do |format|
-      if @record.save
-        format.html { redirect_to @record, notice: 'Record was successfully created.' }
-        format.json { render :show, status: :created, location: @record }
-      else
-        format.html { render :new }
-        format.json { render json: @record.errors, status: :unprocessable_entity }
+module Api
+  module V1
+    class RecordsController < ApplicationBaseController
+      before_action :set_record, only: [:show, :edit, :update, :destroy]
+      def index
+        @records = Record.all
       end
-    end
-  end
 
-  # PATCH/PUT /records/1
-  # PATCH/PUT /records/1.json
-  def update
-    respond_to do |format|
-      if @record.update(record_params)
-        format.html { redirect_to @record, notice: 'Record was successfully updated.' }
-        format.json { render :show, status: :ok, location: @record }
-      else
-        format.html { render :edit }
-        format.json { render json: @record.errors, status: :unprocessable_entity }
+      def show
       end
+
+      def new
+        @record = Record.new
+      end
+
+      def edit
+      end
+
+      def create
+        @record = Record.new(record_params)
+        # music_id = params[:music_id]
+        # @music = Music.create(id: music_id)
+        respond_to do |format|
+          if @record.save
+            format.html { redirect_to @record, notice: 'Record was successfully created.' }
+            format.json { render :show, status: :created, location: @record }
+          else
+            format.html { render :new }
+            format.json { render json: @record.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+
+      def update
+        respond_to do |format|
+          if @record.update(record_params)
+            format.html { redirect_to @record, notice: 'Record was successfully updated.' }
+            format.json { render :show, status: :ok, location: @record }
+          else
+            format.html { render :edit }
+            format.json { render json: @record.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+
+      def destroy
+        @record.destroy
+        respond_to do |format|
+          format.html { redirect_to records_url, notice: 'Record was successfully destroyed.' }
+          format.json { head :no_content }
+        end
+      end
+
+      private
+        def set_record
+          @record = Record.find(params[:id])
+        end
+
+        def record_params
+          params.require(:record).permit(:title, :introduction, :effect, :movie, :dancer_id, :music_id)
+        end
     end
   end
-
-  # DELETE /records/1
-  # DELETE /records/1.json
-  def destroy
-    @record.destroy
-    respond_to do |format|
-      format.html { redirect_to records_url, notice: 'Record was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_record
-      @record = Record.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def record_params
-      params.require(:record).permit(:title, :introduction, :effect, :movie, :dancer_id, :music_id)
-    end
 end
