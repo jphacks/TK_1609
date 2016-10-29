dd6407b7e4463e6e6346e6daf58a7e57cf7e6f07
Yuta Ide <yIde4949@gmail.com>
2016-10-29 11:05:51 +0900

    [update] add association file
     Changes to be committed:
    	modified:   app/models/dancer.rb
    	modified:   app/models/music.rb
    	modified:   app/models/question.rb
    	modified:   app/models/record.rb

--- sadness_ojisan/groove_server/app/models/dancer.rb
+++ sadness_ojisan/groove_server/app/models/dancer.rb
@@ -1,4 +1,11 @@
 class Dancer < ActiveRecord::Base
   authenticates_with_sorcery!
   validates :password, confirmation: true
+  has_many :favorites
+  has_many :records
+  has_many :questions
+  has_many :answers
+  has_many :comments
+  has_many :follows
+  has_many :api_keys
 end
--- sadness_ojisan/groove_server/app/models/record.rb
+++ sadness_ojisan/groove_server/app/models/record.rb
@@ -1,4 +1,6 @@
 class Record < ActiveRecord::Base
   belongs_to :dancer
   belongs_to :music
+  has_many :favorites
+  has_many :comments
 end
