ad3b2d5b97948b9535439ada04066cc3d4bc33c2
Kazunori HORI <horibirds@gmail.com>
2016-12-03 13:10:36 +0900

    a

--- horihori/jphacks.ino
+++ horihori/jphacks.ino
@@ -379,35 +379,14 @@ void setup() {
 // ===                    MAIN PROGRAM LOOP                     ===
 // ================================================================
 void loop() {
-  // if programming failed, don't try to do anything
-  if (!dmpReady) {
-    return;
-  }
-  // wait for MPU interrupt or extra packet(s) available
-  while (!mpuInterrupt && fifoCount < packetSize) {
-  }
-  // reset interrupt flag and get INT_STATUS byte
-  mpuInterrupt = false;
-  mpuIntStatus = mpu.getIntStatus();
-  // get current FIFO count
-  fifoCount = mpu.getFIFOCount();
-  // check for overflow (this should never happen unless our code is too inefficient)
-  if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+  if (false) {
     // reset so we can continue cleanly
     mpu.resetFIFO();
     //        Serial.println(F("FIFO overflow!"));
     // otherwise, check for DMP data ready interrupt (this should happen frequently)
-  } else if (mpuIntStatus & 0x02) {
-    // wait for correct available data length, should be a VERY short wait
-    while (fifoCount < packetSize) {
-      fifoCount = mpu.getFIFOCount();
-    }
+  } else if (true) {
+
     // read a packet from FIFO
-    mpu.getFIFOBytes(fifoBuffer, packetSize);
-    // track FIFO count here in case there is > 1 packet available
-    // (this lets us immediately read more without waiting for an interrupt)
-    fifoCount -= packetSize;
-    mpu.getMotion6(&ax_row, &ay_row, &az_row, &gx_row, &gy_row, &gz_row);
     if (HAND == 1) {
       az_row = -az_row;
       gy_row = -gy_row;
